da380a92f2985865a37e0a8196cdd7a007955716
martinromario55 <martinromario55@gmail.com>
2025-02-14 17:47:52 +0300

    Android Studio Fundamentals - EditText

--- app/src/main/java/com/example/androidfundamentals/MainActivity.kt
+++ app/src/main/java/com/example/androidfundamentals/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.androidfundamentals
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,22 +27,34 @@ class MainActivity : AppCompatActivity() {
 //            Log.d("MainActivity", "${firstName} $lastName, born on $birthDate, from $country just applied to the formula.")
 //        }
 
-        val btnCount = findViewById<Button>(R.id.btnCount)
-        val btnDecrement = findViewById<Button>(R.id.btnDecrement)
-        val btnReset = findViewById<Button>(R.id.btnReset)
-        val tvCount = findViewById<TextView>(R.id.tvCount)
-        var count = 0
-        btnCount.setOnClickListener {
-            count++
-            tvCount.text = "Let's count together: $count"
-        }
-        btnDecrement.setOnClickListener {
-            count--
-            tvCount.text = "Let's count together: $count"
-        }
-        btnReset.setOnClickListener {
-            count = 0
-            tvCount.text = "Let's count together: $count"
+//        val btnCount = findViewById<Button>(R.id.btnCount)
+//        val btnDecrement = findViewById<Button>(R.id.btnDecrement)
+//        val btnReset = findViewById<Button>(R.id.btnReset)
+//        val tvCount = findViewById<TextView>(R.id.tvCount)
+//        var count = 0
+//        btnCount.setOnClickListener {
+//            count++
+//            tvCount.text = "Let's count together: $count"
+//        }
+//        btnDecrement.setOnClickListener {
+//            count--
+//            tvCount.text = "Let's count together: $count"
+//        }
+//        btnReset.setOnClickListener {
+//            count = 0
+//            tvCount.text = "Let's count together: $count"
+//        }
+
+        val btnAdd = findViewById<Button>(R.id.btnAdd)
+        val tvResult = findViewById<TextView>(R.id.tvResult)
+        val etNum1 = findViewById<EditText>(R.id.etFirstNum)
+        val etNum2 = findViewById<EditText>(R.id.etSecondNum)
+
+        btnAdd.setOnClickListener {
+            val num1 = etNum1.text.toString().toIntOrNull() ?:0
+            val num2 = etNum2.text.toString().toIntOrNull() ?:0
+            val result = num1 + num2
+            tvResult.text = result.toString()
         }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
