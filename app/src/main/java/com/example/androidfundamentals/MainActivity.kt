8538dd03a1715cb1ce74c9944e554f262003f954
martinromario55 <martinromario55@gmail.com>
2025-02-14 16:40:44 +0300

    Android Studio Fundamentals - TextViews

--- app/src/main/java/com/example/androidfundamentals/MainActivity.kt
+++ app/src/main/java/com/example/androidfundamentals/MainActivity.kt
@@ -1,14 +1,12 @@
 package com.example.androidfundamentals
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import java.util.Collections
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,15 +14,33 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        val btnApply = findViewById<Button>(R.id.btnApply)
+//        val btnApply = findViewById<Button>(R.id.btnApply)
+//
+//        btnApply.setOnClickListener {
+//            val firstName = findViewById<TextView>(R.id.etFirstName).text.toString()
+//            val lastName = findViewById<TextView>(R.id.etLastName).text.toString()
+//            val birthDate = findViewById<TextView>(R.id.etDateOfBirth).text.toString()
+//            val country = findViewById<TextView>(R.id.etCountry).text.toString()
+//
+//            Log.d("MainActivity", "${firstName} $lastName, born on $birthDate, from $country just applied to the formula.")
+//        }
 
-        btnApply.setOnClickListener {
-            val firstName = findViewById<TextView>(R.id.etFirstName).text.toString()
-            val lastName = findViewById<TextView>(R.id.etLastName).text.toString()
-            val birthDate = findViewById<TextView>(R.id.etDateOfBirth).text.toString()
-            val country = findViewById<TextView>(R.id.etCountry).text.toString()
-
-            Log.d("MainActivity", "${firstName} $lastName, born on $birthDate, from $country just applied to the formula.")
+        val btnCount = findViewById<Button>(R.id.btnCount)
+        val btnDecrement = findViewById<Button>(R.id.btnDecrement)
+        val btnReset = findViewById<Button>(R.id.btnReset)
+        val tvCount = findViewById<TextView>(R.id.tvCount)
+        var count = 0
+        btnCount.setOnClickListener {
+            count++
+            tvCount.text = "Let's count together: $count"
+        }
+        btnDecrement.setOnClickListener {
+            count--
+            tvCount.text = "Let's count together: $count"
+        }
+        btnReset.setOnClickListener {
+            count = 0
+            tvCount.text = "Let's count together: $count"
         }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
