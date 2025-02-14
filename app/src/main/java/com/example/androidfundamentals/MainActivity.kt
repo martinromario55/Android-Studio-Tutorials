8d6c3f0cf0a1b82d1642454780a25bc6e51ca079
martinromario55 <martinromario55@gmail.com>
2025-02-14 11:35:20 +0300

    Android Studio Fundamentals - Buttons

--- app/src/main/java/com/example/androidfundamentals/MainActivity.kt
+++ app/src/main/java/com/example/androidfundamentals/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.androidfundamentals
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,25 +16,21 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        val list = listOf(5,3,6,7,9,1)
-        sortList(list)
-        Log.d("MainActivity", "Hello, this is our first log message")
-        println(list)
+        val btnApply = findViewById<Button>(R.id.btnApply)
+
+        btnApply.setOnClickListener {
+            val firstName = findViewById<TextView>(R.id.etFirstName).text.toString()
+            val lastName = findViewById<TextView>(R.id.etLastName).text.toString()
+            val birthDate = findViewById<TextView>(R.id.etDateOfBirth).text.toString()
+            val country = findViewById<TextView>(R.id.etCountry).text.toString()
+
+            Log.d("MainActivity", "${firstName} $lastName, born on $birthDate, from $country just applied to the formula.")
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-    }
-
-    private fun sortList(list: List<Int>) {
-        for (i in 0..list.size - 1) {
-            for (j in 0..list.size - 2) {
-                if (list[j] > list[j + 1]) {
-                    Collections.swap(list, j, j + 1)
-                }
-            }
         }
     }
 
