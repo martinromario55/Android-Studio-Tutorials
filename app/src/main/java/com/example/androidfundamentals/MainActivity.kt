45b607df78fc8ad17f1a976edfaa7f8bc046750a
martinromario55 <martinromario55@gmail.com>
2025-02-14 08:47:03 +0300

    Android Studio Fundamentals - Error handling

--- app/src/main/java/com/example/androidfundamentals/MainActivity.kt
+++ app/src/main/java/com/example/androidfundamentals/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.example.androidfundamentals
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.Collections
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val list = listOf(5,3,6,7,9,1)
+        sortList(list)
+        Log.d("MainActivity", "Hello, this is our first log message")
+        println(list)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -18,12 +26,15 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onPause() {
-        super.onPause()
-        println("onPause")
+    private fun sortList(list: List<Int>) {
+        for (i in 0..list.size - 1) {
+            for (j in 0..list.size - 2) {
+                if (list[j] > list[j + 1]) {
+                    Collections.swap(list, j, j + 1)
+                }
+            }
+        }
     }
 
-    override fun onRestart() {
-        super.onRestart()
-    }
+
 }
